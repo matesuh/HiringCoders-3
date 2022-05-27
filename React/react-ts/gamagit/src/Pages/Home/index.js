ddd74283983bb55c859ae5b6581b87f86309f08e
Matheus <msbdacosta@hotmail.com>
2022-05-27 14:33:12 -0300

    updated /repositories and fixed error on reload

--- React/react-ts/gamagit/src/Pages/Home/index.js
+++ React/react-ts/gamagit/src/Pages/Home/index.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import * as S from './styled'
+import { useNavigate } from 'react-router-dom'
 
 function App(props) {
+    const navigate = useNavigate()
     const [usuario, setUsuario] = useState('')
 
-    function handlePesquisa() {}
-    axios
-        .get(`https://api.github.com/users/${usuario}/repos`)
-        .then(response => {
-            const repositories = response.data
-            const repositoriesName = []
-            repositories.map(repository =>
-                repositoriesName.push(repository.name)
-            )
-            localStorage.setItem(
-                'repositoriesName',
-                JSON.stringify(repositoriesName)
-            )
-        })
+    function handlePesquisa() {
+        axios
+            .get(`https://api.github.com/users/${usuario}/repos`)
+            .then(response => {
+                const repositories = response.data
+                const repositoriesName = []
+                repositories.map(repository =>
+                    repositoriesName.push(repository.name)
+                )
+                localStorage.setItem(
+                    'repositoriesName',
+                    JSON.stringify(repositoriesName)
+                )
+                navigate('/repositories')
+            })
+    }
 
     return (
         <S.Container>
